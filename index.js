509cfe807fc8bdb7a619d03d26b8a0ed5b9592d3
Ahmar khan <ahmarbilal223344@gmail.com>
2023-01-02 17:02:01 +0500

    done

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const hourSelect = document.getElementById("hourSelect");
 const minSelect = document.getElementById("minSelect");
-const secSelect = document.getElementById("secSelect");
 const apmpSel = document.getElementById("apmpSel");
 const button = document.getElementById("button");
 const selectEl = document.getElementsByTagName("select");
 const h1 = document.getElementById("h1");
 let time;
+let isAlarmSet = false;
+let sound = new Audio("ringtone.mp3");
+
 for (let i = 1; i <= 12; i++) {
   const optionEl = document.createElement("option");
   optionEl.innerText = i < 10 ? "0" + i : i;
@@ -16,12 +18,6 @@ for (let i = 1; i <= 60; i++) {
   const optionEl = document.createElement("option");
   optionEl.innerText = i < 10 ? "0" + i : i;
   minSelect.appendChild(optionEl);
-}
-
-for (let i = 1; i <= 60; i++) {
-  const optionEl = document.createElement("option");
-  optionEl.innerText = i < 10 ? "0" + i : i;
-  secSelect.appendChild(optionEl);
 }
 
 setInterval(() => {
@@ -43,15 +39,25 @@ setInterval(() => {
 
   h1.innerText = `${h} : ${m} : ${s} ${ampm}`;
 
-  if (time == h1.innerText) {
-    console.log("ringing");
+  if (time == `${h}:${m} ${ampm}`) {
+    sound.play();
+    sound.loop = true;
   }
 }, 1000);
 
 button.addEventListener("click", () => setAlarm());
 
 function setAlarm() {
-  time = `${selectEl[0].value} : ${selectEl[1].value} : ${selectEl[2].value} ${selectEl[3].value}`;
+  if (isAlarmSet) {
+    minSelect.classList.remove("disable");
+    hourSelect.classList.remove("disable");
+    apmpSel.classList.remove("disable");
+    button.innerText = "Set Alarm";
+    sound.pause();
+    return (isAlarmSet = false);
+  }
+  time = `${selectEl[0].value}:${selectEl[1].value} ${selectEl[2].value}`;
+  console.log(time);
   if (
     time.includes("Hour") ||
     time.includes("Minutes") ||
@@ -61,7 +67,8 @@ function setAlarm() {
   } else {
     minSelect.classList.add("disable");
     hourSelect.classList.add("disable");
-    secSelect.classList.add("disable");
     apmpSel.classList.add("disable");
   }
+  isAlarmSet = true;
+  button.innerText = "Reset Alarm";
 }
